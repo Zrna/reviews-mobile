ebd7e1101651410db589fd35c19e7819c5acacae
zrna <lukazrnic5@gmail.com>
2024-07-10 18:45:47 +0200

    rename the containerClassName prop to class to support Tailwind autocompetion

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -5,17 +5,17 @@ interface CustomButtonProps {
   isLoading?: boolean;
   isDisabled?: boolean;
   isFullWidth?: boolean;
-  containerClassName?: string;
+  class?: string;
   onPress?: () => void;
 }
 
-const CustomButton = ({ containerClassName, text, isLoading, isDisabled, isFullWidth, onPress }: CustomButtonProps) => {
+const CustomButton = ({ class: customClass, text, isLoading, isDisabled, isFullWidth, onPress }: CustomButtonProps) => {
   return (
     <TouchableOpacity
       disabled={isDisabled || isLoading}
       onPress={onPress}
       activeOpacity={0.7}
-      className={`bg-primary rounded-xl min-h-[62px] justify-center items-center ${isFullWidth ? "w-full" : ""} ${isLoading || isDisabled ? "opacity-60" : ""} ${containerClassName}`}
+      className={`bg-primary rounded-xl min-h-[62px] justify-center items-center ${isFullWidth ? "w-full" : ""} ${isLoading || isDisabled ? "opacity-60" : ""} ${customClass}`}
     >
       <Text className="text-black font-psemibold text-lg">{text}</Text>
     </TouchableOpacity>
