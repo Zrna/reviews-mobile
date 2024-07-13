b481d4a3f3df0a415fdeda5cd7c3b83eaaa2d0d0
zrna <lukazrnic5@gmail.com>
2024-07-13 09:49:22 +0200

    add the CustomInput component and implement login scren UI

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,11 +1,66 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
-import { Text } from "react-native";
+import { useForm } from "react-hook-form";
+import { Image, ScrollView, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { z } from "zod";
+
+import logo from "@/assets/images/logo.png";
+import { CustomButton, CustomInput } from "@/components";
+
+const LoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6, "Password must contain at least 6 characters"),
+});
+
+type LoginProps = z.infer<typeof LoginSchema>;
 
 const Login = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<LoginProps>({ resolver: zodResolver(LoginSchema) });
+
+  const handleLogin = async (data: any) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(data);
+  };
+
   return (
-    <SafeAreaView className="h-full justify-center items-center bg-black">
-      <Text className="text-white">Login</Text>
+    <SafeAreaView className="h-full bg-black">
+      <ScrollView>
+        <View className="items-center">
+          <Image source={logo} className="w-[115px] h-[35px] mt-4" resizeMode="contain" />
+          <View className="w-full min-h-[85vh] justify-center px-4">
+            <CustomInput
+              control={control}
+              name="email"
+              placeholder="Email"
+              label="Email"
+              keyboardType="email-address"
+              textContentType="emailAddress"
+              autoComplete="email"
+            />
+            <CustomInput
+              control={control}
+              textContentType="password"
+              name="password"
+              placeholder="Password"
+              label="Password"
+              secureTextEntry
+            />
+            <CustomButton
+              text="Log in"
+              onPress={handleSubmit(handleLogin)}
+              isFullWidth
+              isDisabled={isSubmitting}
+              isLoading={isSubmitting}
+              class="mt-4"
+            />
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
---
+++ components/CustomInput.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Control, Controller, FieldValues, RegisterOptions } from "react-hook-form";
+import { Text, TextInput, TextInputProps, TouchableOpacity, View } from "react-native";
+import { Eye, EyeOff } from "react-native-feather";
+
+interface CustomInputProps extends TextInputProps {
+  name: string;
+  label?: string;
+  control: Control<any>;
+  rules?: Pick<RegisterOptions<FieldValues>, "maxLength" | "minLength" | "validate" | "required">;
+  secureTextEntry?: boolean;
+  Icon?: React.ReactElement<{ color: string; width?: number; height?: number }>;
+}
+
+const CustomInput: React.FC<CustomInputProps> = ({
+  control,
+  name,
+  rules = {},
+  label,
+  secureTextEntry = false,
+  Icon,
+  ...props
+}) => {
+  const [isValueHidden, setShowIsValueHidden] = useState(secureTextEntry);
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      render={({ field: { value, onChange, onBlur }, fieldState: { error } }) => {
+        return (
+          <View className="w-full space-y-1 mb-4">
+            <View className="space-y-1">
+              {label && <Text className="text-dimmed font-pop-medium">{label}</Text>}
+              <View
+                className={`flex-row justify-between bg-dark text-white items-center px-3 rounded-xl h-16 border border-light-dark focus:border-primary ${error ? "border border-red-500" : ""}`}
+              >
+                {Icon && React.cloneElement(Icon, { width: 24, height: 24, color: error ? "#ef4444" : "#D0D0D0" })}
+                <TextInput
+                  value={value}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  secureTextEntry={secureTextEntry && isValueHidden}
+                  placeholderTextColor="#D0D0D0"
+                  className={`text-white flex-1 text-base ${Icon ? "ml-2" : ""}`}
+                  {...props}
+                />
+                {secureTextEntry && (
+                  <TouchableOpacity onPress={() => setShowIsValueHidden(!isValueHidden)}>
+                    {isValueHidden ? (
+                      <Eye color="#D0D0D0" width={20} height={20} />
+                    ) : (
+                      <EyeOff color="#D0D0D0" width={20} height={20} />
+                    )}
+                  </TouchableOpacity>
+                )}
+              </View>
+            </View>
+            {error && <Text className="text-red-500">{error.message}</Text>}
+          </View>
+        );
+      }}
+    />
+  );
+};
+
+export default CustomInput;
