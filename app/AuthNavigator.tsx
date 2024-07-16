b91dab5f07ec9ce5bbf6349cff022cf9e3a3710f
zrna <lukazrnic5@gmail.com>
2024-07-16 18:02:06 +0200

    remove console.logs

--- app/AuthNavigator.tsx
+++ app/AuthNavigator.tsx
@@ -4,17 +4,14 @@ import { useAuthContext } from "@/contexts/AuthContext";
 
 export const AuthNavigator = () => {
   const { authState } = useAuthContext();
-  console.log(authState);
 
   if (authState.isLoggedIn) {
-    console.log("logged in stack");
     return (
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(protected)" />
       </Stack>
     );
   } else {
-    console.log("logged out stack");
     return (
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(auth)" />
