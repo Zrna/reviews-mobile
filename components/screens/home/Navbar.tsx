b455574e63ff4815269618d80bb69f7482c5685f
zrna <lukazrnic5@gmail.com>
2024-08-04 13:41:03 +0200

    add showSearch prop to navbar

--- components/screens/home/Navbar.tsx
+++ components/screens/home/Navbar.tsx
@@ -5,16 +5,22 @@ import { Search } from "react-native-feather";
 
 import logoSmall from "@/assets/images/logo-small.png";
 
-const Navbar = () => {
+interface NavbarProps {
+  showSearch?: boolean;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ showSearch = true }) => {
   return (
     <View
       className="flex-row items-center justify-between py-3 px-1 w-full"
       style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
     >
       <Image source={logoSmall} className="w-14 h-6" resizeMode="contain" />
-      <Link href="search/[query]">
-        <Search stroke="white" width={24} height={24} />
-      </Link>
+      {showSearch && (
+        <Link href="search/[query]">
+          <Search stroke="white" width={24} height={24} />
+        </Link>
+      )}
     </View>
   );
 };
