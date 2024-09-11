099338b1105b5621f0fc99d5316c24b1138a90d8
zrna <lukazrnic5@gmail.com>
2024-09-11 12:04:22 +0200

    improve the ReviewCard to support youtube and spotify logos

--- components/screens/home/ReviewCard.tsx
+++ components/screens/home/ReviewCard.tsx
@@ -5,6 +5,8 @@ import { Image, Text, View } from "react-native";
 
 import defaultPoster from "@/assets/images/default-poster.png";
 import { Review } from "@/interfaces/reviews";
+import { streamingAppsMap } from "@/utils/platforms";
+import { getUrlDomain } from "@/utils/url";
 
 interface ReviewCardProps {
   review: Review;
@@ -12,13 +14,27 @@ interface ReviewCardProps {
 }
 
 const ReviewCard: React.FC<ReviewCardProps> = ({ review, isBig = false }) => {
-  const imageSource = review.img ? { uri: `data:image/jpeg;base64,${review.img}` } : defaultPoster;
+  const urlDomain = review.url && getUrlDomain(review.url);
+  const imageSource = review.img
+    ? { uri: `data:image/jpeg;base64,${review.img}` }
+    : urlDomain
+      ? streamingAppsMap[urlDomain].img
+      : defaultPoster;
+
+  const showPlatformLogo = !review.img && (urlDomain === "youtube" || urlDomain === "spotify");
+
   const cardSize = isBig ? "w-[200px] h-[300px]" : "w-[110px] h-[160px]";
 
   return (
     <Link href={`/reviews/${review.id}`}>
-      <View className={`${cardSize} rounded-md`}>
-        <Image source={imageSource} className="w-full h-full rounded-md" resizeMode="cover" />
+      <View
+        className={`${cardSize} rounded-md flex justify-center items-center ${showPlatformLogo ? "border border-light-dark" : ""}`}
+      >
+        <Image
+          source={imageSource}
+          className={`${showPlatformLogo ? "w-12 h-12" : "w-full h-full"} rounded-md`}
+          resizeMode={showPlatformLogo ? "contain" : "cover"}
+        />
         <LinearGradient
           colors={["transparent", "rgba(0,0,0,0.7)"]}
           className="absolute bottom-0 w-full p-2 rounded-b-md"
