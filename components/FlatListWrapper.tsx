9cf936237f0c6768d8ad582ed607569e4e2910e5
zrna <lukazrnic5@gmail.com>
2024-09-24 13:28:58 +0200

    add the onPress/see all to the FlatListWrapper component

--- components/FlatListWrapper.tsx
+++ components/FlatListWrapper.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 interface FlatListWrapperProps {
   class?: string;
   title?: string;
   children: React.ReactNode;
+  onPress?: () => void;
 }
 
-const FlatListWrapper: React.FC<FlatListWrapperProps> = ({ class: customClass, children, title }) => {
+const FlatListWrapper: React.FC<FlatListWrapperProps> = ({ class: customClass, children, title, onPress }) => {
   return (
     <View className={`space-y-2 ${customClass}`}>
-      <Text className="text-white font-pop-bold text-xl">{title}</Text>
+      <TouchableOpacity
+        className="flex flex-row items-center justify-between"
+        onPress={onPress}
+        activeOpacity={onPress ? 0.5 : 1}
+      >
+        <Text className="text-white font-pop-bold text-xl">{title}</Text>
+        {onPress && <Text className="text-primary font-pop-light text-sm">See all</Text>}
+      </TouchableOpacity>
       {children}
     </View>
   );
