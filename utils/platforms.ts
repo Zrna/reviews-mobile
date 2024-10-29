ecbc519be6a4d578210f36b419b2ab6e5e5c82e4
zrna <lukazrnic5@gmail.com>
2024-10-29 11:38:08 +0100

    add max streaming platform logo

--- utils/platforms.ts
+++ utils/platforms.ts
@@ -1,6 +1,6 @@
 import appleTvLogo from "@/assets/images/platforms/apple-tv.png";
 import disneyPlusLogo from "@/assets/images/platforms/disneyplus.png";
-import hboMaxLogo from "@/assets/images/platforms/hbo-max.png";
+import maxLogo from "@/assets/images/platforms/max.png";
 import netflixLogo from "@/assets/images/platforms/netflix.png";
 import spotifyLogo from "@/assets/images/platforms/spotify.png";
 import youtubeLogo from "@/assets/images/platforms/youtube.png";
@@ -11,8 +11,12 @@ export const streamingAppsMap: { [key: string]: { link: string; img: any } } = {
     img: netflixLogo,
   },
   hbomax: {
-    link: "https://play.hbomax.com/",
-    img: hboMaxLogo,
+    link: "https://play.max.com/",
+    img: maxLogo,
+  },
+  max: {
+    link: "https://play.max.com/",
+    img: maxLogo,
   },
   disneyplus: {
     link: "https://www.disneyplus.com",
