e4a550ceefe22860bfc989f1b7348b7e7291a80b
zrna <lukazrnic5@gmail.com>
2026-04-30 16:45:07 +0200

    change landing screen copy

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { Link, Redirect, router } from "expo-router";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import logo from "@/assets/images/logo.png";
@@ -16,26 +16,29 @@ const App = () => {
 
   return (
     <SafeAreaView className="h-full bg-black">
-      <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="items-center">
-          <Image source={logo} className="w-[130px] h-[50px] mt-4" resizeMode="contain" />
-          <View className="relative flex items-center justify-center w-full h-[80vh] px-6">
-            <Image source={bg} className="absolute opacity-30" />
-            <View className="z-10 gap-10 py-6">
-              <Text className="text-3xl text-white font-pop-bold text-center">
-                Simplify your entertainment with <Text className="text-primary text-4xl">moovier</Text>
-              </Text>
-              <Text className="text-dimmed text-sm font-pop-regular text-center">
-                Keep all your ratings in one place. Save your thoughts, get new recommendations, and more.
-              </Text>
-            </View>
-            <CustomButton text="Continue to Login" isFullWidth onPress={() => router.push("/login")} />
-            <Link href="/register" className="text-dimmed mt-6">
+      <View className="items-center">
+        <Image source={logo} className="w-[130px] h-[50px] mt-4 z-10" resizeMode="contain" />
+        <View className="relative flex items-center justify-center w-full h-full px-4 gap-8">
+          <Image source={bg} className="absolute opacity-30" />
+          <View className="gap-6">
+            <Text className="text-6xl text-white font-pop-bold">A diary for everything you watch.</Text>
+            <Text className="text-dimmed text-lg font-pop-regular">
+              You just finished it. Was it any good? Write it down now, before the next thing buries it.
+            </Text>
+          </View>
+          <View className="gap-4 w-full items-center">
+            <CustomButton
+              variant="primary"
+              text="Continue to Login"
+              isFullWidth
+              onPress={() => router.push("/login")}
+            />
+            <Link href="/register" className="text-dimmed">
               or create an account
             </Link>
           </View>
         </View>
-      </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
